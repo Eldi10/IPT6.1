e7408cdda710835d169a556f35458765908647d7
Eldi10 <Eldi_Kryeziu@sluz.ch>
2026-01-22 15:16:22 +0100

    Add SQLite database and C# code

--- BD-C-sharp-connection/BD-C-sharp-connection/Program.cs
+++ BD-C-sharp-connection/BD-C-sharp-connection/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+class Program
+{
+    static void Main()
+    {
+        // EXAKT deine DB
+        string path = @"C:\Code\IPT6.1\BD-C-sharp-connection\Schule.db";
+        string connectionString = $"Data Source={path}";
+
+        using var con = new SqliteConnection(connectionString);
+        con.Open();
+
+        var cmd = con.CreateCommand();
+        cmd.CommandText = "SELECT id, name FROM users;";
+
+        using var reader = cmd.ExecuteReader();
+        while (reader.Read())
+        {
+            int id = reader.GetInt32(0);      // id = INTEGER
+            string name = reader.GetString(1); // name = TEXT
+
+            Console.WriteLine($"{id} | {name}");
+        }
+
+        Console.WriteLine("Fertig!");
+        Console.ReadLine();
+    }
+}
